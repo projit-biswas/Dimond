413d30508d1d5707f50c46e41e5e64ab3b2d31a9
Projit Biswas <projit.biswas99@gmail.com>
2024-04-15 11:26:10 +0530

    button added

--- src/component/Homepage/HeroSection.jsx
+++ src/component/Homepage/HeroSection.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import "./style.css"
 
 function HeroSection() {
+
+  const handelClick = () => {
+    console.log(e.target);
+  }
+
   return (
     <>
         <section className="w-full h-full font-pop absolute top-0 left-0 text-white hero">
@@ -9,6 +14,14 @@ function HeroSection() {
                 <h1 className='text-[48px] leading-[60px] font-enco'>W E L C O M E</h1>
                 <p className='text-[40px] leading-[47px]'>Medallist & Gift Makers to Royalties & Heads of State</p>
             </div>
+            <div className='relative top-[710px] left-[128px]  inline-block'>
+              <button 
+              className='decoration-solid'
+              onClick={handelClick}>
+                <span
+                className='text-[30px] decoration-solid'>Discover more</span>
+                </button>
+              </div>
 
 
         </section>
